98c0b41ce5e0e5fa0d94e023f7dad6cf758d9f37
lirajw <l.lirajw@hotmail.com>
2021-12-02 07:34:28 -0300

    fist commit

---
+++ src/components/SubscribeButton/index.tsx
@@ -0,0 +1,17 @@
+import style from './styles.module.scss'
+
+interface SubscribeButtonProps {
+    priceId: string;
+}
+
+export function SubscribeButton({ priceId }: SubscribeButtonProps) {
+    return (
+        <button
+        type="button"
+        className={style.subscribeButton}
+        >
+            Subscribe Now!
+
+        </button>
+    )
+}
